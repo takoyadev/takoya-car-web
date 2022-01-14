e7108d453db532adc4df1a51b106d882b9e3109e
chinjto <contact@cyril-defaye.fr>
2022-01-14 15:53:25 +0100

    Feature Cars Grid
    Changement de la route pour le module cars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {CarListComponent} from "./car/list/car-list.component";
 import {MaintenanceListComponent} from "./maintenance/list/maintenance-list.component";
 import {HomeComponent} from "./core/home/home.component";
 import {OwnerListComponent} from "./owner/list/owner-list.component";
+import {CarGridComponent} from "./car/grid/car-grid.component";
 
 const routes: Routes = [
-  { path: 'cars', component: CarListComponent },
+  // { path: 'cars', component: CarListComponent },
+  { path: 'cars', component: CarGridComponent },
   { path: 'maintenances', component: MaintenanceListComponent },
   { path: 'owners', component: OwnerListComponent },
   { path: '', component: HomeComponent },
